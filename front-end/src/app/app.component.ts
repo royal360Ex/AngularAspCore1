1d461c49f87225ca0650550e06e65415554a3bf3
Jorge Pardo <jpardo@codema.coop>
2024-07-05 16:58:02 -0500

    avance ejecicio 1

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  peliculas;
+  ngOnInit(): void {
+
+    setTimeout(() => {
+      this.peliculas = [{
+        titulo: 'Spider Man',
+        fechalanzamiento: new Date(),
+        precio:1400.99
+    
+      },
+      {
+        titulo: 'Moana',
+        fechalanzamiento: new Date('2016-11-14'),
+        precio:400.99
+      }]  
+    }, 5000);
+    
+  }
   title = 'front-end';
+  
+
+
 }
